a136f6665ff4cab712bcbf7693158a163100aaba
Thiago Sousa Santos <thiagos.xsantos@gmail.com>
2019-12-09 21:08:12 -0300

    CHANGED: agora modelos tem validação

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
   has_many :contacts
   has_many :sent, class_name: 'Message', foreign_key: 'sender_id'
   has_many :received, class_name: 'Message', foreign_key: 'receiver_id'
+  validates :name, presence: true
 end
