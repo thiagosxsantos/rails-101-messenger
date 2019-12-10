a5613e036e4686f9071a16a79f681f9a4c3be442
Thiago Sousa Santos <thiagos.xsantos@gmail.com>
2019-12-09 23:58:11 -0300

    ADDED: default layout and sidebar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,9 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import bulmaQuickview from 'bulma-quickview/src/js'
+
+document.addEventListener('turbolinks:load', function() {
+    let quickviews = bulmaQuickview.attach()
+})
