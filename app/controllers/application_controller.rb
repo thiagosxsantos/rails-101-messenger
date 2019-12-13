4bf6a83d6dacc61a28dbb88d5e5190cae4da3feb
Thiago Sousa Santos <thiagos.xsantos@gmail.com>
2019-12-13 10:38:28 -0300

    ADDED: account update page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,6 @@ class ApplicationController < ActionController::Base
   protected
   def configure_devise_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:photo, :name, :email, :password, :password_confirmation])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,8 @@ class User < ApplicationRecord
   has_many :contacts
   has_many :sent, class_name: 'Message', foreign_key: 'sender_id'
   has_many :received, class_name: 'Message', foreign_key: 'receiver_id'
+
+  has_one_attached :photo
+
   validates :name, presence: true
 end
