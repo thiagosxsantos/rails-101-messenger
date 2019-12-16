aac94cf44aa77bfcd04ee2255911eb4507c08040
Thiago Sousa Santos <thiagos.xsantos@gmail.com>
2019-12-16 18:06:24 -0300

    ADDED: account contacts list

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
+
+  resources :contacts, only: %i(index new create destroy)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ test/controllers/contatcs_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ContatcsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
